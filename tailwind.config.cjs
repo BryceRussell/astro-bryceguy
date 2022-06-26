a8482036b3cc08f97b0dbdf3d9f6b052c2da1e2e
BryceRussell <brycetrussell@gmail.com>
2022-06-26 00:16:02 -0500

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./demo/src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}', './packages/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
