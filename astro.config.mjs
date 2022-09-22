51e5c9ab42155930ae50ca164a4e3ba7839054c6
BryceRussell <brycetrussell@gmail.com>
2022-09-22 13:14:59 -0500

    Update Astro
    - Update package.json to Astro ^v1.0.0
    - Moved root flags to config
    - Update demo pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  root: './demo',
+  srcDir: './demo/src',
   integrations: [tailwind()],
   vite: {
     ssr: {
       external: ["svgo"],
+      noExternal: ["astro-json-element", "astro-xelement"],
     },
   },
 });
