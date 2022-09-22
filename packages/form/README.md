890685a56c50b962d5e20d7f0d2957e7a27f0ae9
BryceRussell <brycetrussell@gmail.com>
2022-09-22 13:18:28 -0500

    inital commit form package
    Mostly a proof of concept, will probably delete in favor of wrapping a web component

---
+++ packages/form/README.md
@@ -0,0 +1,3 @@
+# `@astro-bryceguy/form`
+
+This component is just a proof of concept, probably wont use in favor of wrapping a web component
---
+++ packages/form/index.mjs
@@ -0,0 +1,3 @@
+export { default as Form } from './Form.astro';
+export { default as Input } from './Input.astro';
+export { default as Fieldset } from './Fieldset.astro';
